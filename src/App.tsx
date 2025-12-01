d462995f24d2730e6eac67f13bcc77c01a27021e
joseph <heegu0311@gmail.com>
2025-12-02 01:33:35 +0900

    feat: SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { LinearCalendar2026 } from "./components/LinearCalendar2026.tsx";
+import { LinearCalendar2026 } from "./components/LinearCalendar2026";
 import { useAnalytics } from "./hooks/useAnalytics.js";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export default function App() {
   useAnalytics();
 
   return (
-    <div className="min-h-screen bg-[#FFF5F0] p-4 md:p-8">
-      <LinearCalendar2026 />
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>홈페이지 타이틀</title>
+        <meta name="description" content="habit tracker" />
+        <meta property="og:title" content="havit - have your habit" />
+        <meta
+          property="og:description"
+          content="Achieve your goals with habits 2026"
+        />
+      </Helmet>
+      <div className="min-h-screen bg-[#FFF5F0] p-4 md:p-8">
+        <LinearCalendar2026 />
+      </div>
+    </HelmetProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://yourdomain.com", // 본인의 도메인 입력
+      // 경로 수동 추가 가능
+    }),
+  ],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
