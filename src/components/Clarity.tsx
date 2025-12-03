9455950b8e3afd6cad7ed518efd4ad0290ca8256
joseph <heegu0311@gmail.com>
2025-12-03 23:35:08 +0900

    feat: Clarity

--- src/components/Clarity.tsx
+++ src/components/Clarity.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import Clarity from '@microsoft/clarity';
+import { useEffect } from "react";
+import Clarity from "@microsoft/clarity";
 
 const ClarityInitializer = () => {
   useEffect(() => {
     // 실제 Clarity 프로젝트 ID로 대체하세요.
-    Clarity.init(process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID || '');
+    Clarity.init(process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID || "ues68jf7z3");
   }, []);
   return <></>;
 };
